e93c1499c908af94546d1b554c823a972756edf6
Rudy <RodriguezLucha@gmail.com>
2019-03-14 23:22:04 -0700

    Finally got an SF map rendering in D3 version 5

--- src/main.js
+++ src/main.js
@@ -1,23 +1,37 @@
-import * as d3 from 'd3';
+let width = 600,
+  height = 400;
 
-let svg = d3.select('#chart-area')
-  .append('svg')
-  .attr('width', '400')
-  .attr('height', '400');
+let projection = d3.geoConicEqualArea()
+  .scale(153)
+  .translate([width / 2, height / 2])
+  .precision(.1);
 
-d3.json('data/buildings.json').then((data) => {
-  console.log(data);
+let path = d3.geoPath()
+  .projection(projection);
 
-  data.forEach((d) => {
-    d.height = +d.height;
-  });
+let graticule = d3.geoGraticule();
 
-  let rects = svg.selectAll('rect')
-    .data(data)
-    .enter().append('rect')
-    .attr('y', 0)
-    .attr('x', (d, i) => (i * 60))
-    .attr('width', 40)
-    .attr('height', (d) => d.height)
-    .attr('fill', (d) => 'grey');
+let svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+d3.json('data/sf.topojson', (error, world) => {
+  if (error) throw error;
+
+  svg.append('path')
+    .datum(topojson.feature(world, world.objects.land))
+    .attr('class', 'land')
+    .attr('d', path);
+
+  svg.append('path')
+    .datum(topojson.mesh(world, world.objects.countries, (a, b) => a !== b))
+    .attr('class', 'boundary')
+    .attr('d', path);
+
+  svg.append('path')
+    .datum(graticule)
+    .attr('class', 'graticule')
+    .attr('d', path);
 });
+
+d3.select(self.frameElement).style('height', `${height  }px`);
